ea372bc8022f27c8f81bffd30a7889cbdbd0dba9
rathardashing <73247196+rathardashing@users.noreply.github.com>
2020-10-21 17:04:44 -0400

    Maybe added dragon scale support
    just trying something out to see if it fixes the dragon scale bug

--- CookieMonster.js
+++ CookieMonster.js
@@ -3239,6 +3239,7 @@ CM.Sim.CalculateGains = function() {
 	if (CM.Sim.Has('A lump of coal')) mult *= 1.01;
 	if (CM.Sim.Has('An itchy sweater')) mult *= 1.01;
 	if (CM.Sim.Has('Santa\'s dominion')) mult *= 1.2;
+	if (CM.Sim.Has('Dragon scale')) mult *= 1.03;
 
 	if (CM.Sim.Has('Fortune #100')) mult *= 1.01;
 	if (CM.Sim.Has('Fortune #101')) mult *= 1.07;
